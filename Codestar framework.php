2e1120cd574254ad44230b4de1931bd3114070ed
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-03-14 13:51:17 +0600

    Create

---
+++ Codestar framework.php
@@ -0,0 +1,9 @@
+<?php
+Documentation :  https://codestarframework.com/documentation/
+
+// ============================================
+//                    Notes
+// ============================================
+Free version has only 'Admin Option' 
+other options are premium
+  
