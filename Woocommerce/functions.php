23e0f930862bace64890f9b270db23a3e1bba86c
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-10-21 21:49:26 +0600

    Update functions.php

--- Woocommerce/functions.php
+++ Woocommerce/functions.php
@@ -4,6 +4,42 @@
 
 
 // ==========================
+// Give woocommerce support ***
+// ==========================
+function  organi_setup_theme()
+{
+
+    add_theme_support('woocommerce');   // we must give woo support
+	or
+    add_theme_support( 'woocommerce', array(
+	'thumbnail_image_width' => 150,
+	'single_image_width'    => 300,
+	        'product_grid'          => array(
+	            'default_rows'    => 3,
+	            'min_rows'        => 2,
+	            'max_rows'        => 8,
+	            'default_columns' => 4,
+	            'min_columns'     => 2,
+	            'max_columns'     => 5,
+	        ),
+	) );
+	
+    add_theme_support( 'wc-product-gallery-zoom' );   // Image Zoom/Magnification
+
+    add_theme_support( 'wc-product-gallery-slider' );  //Image Slider
+
+    add_theme_support( 'wc-product-gallery-lightbox' );  // Lightbox
+
+}
+add_action('after_setup_theme', 'organi_setup_theme');
+
+
+
+
+
+
+
+// ==========================
 // Shop/archive page
 // ==========================
 /**
