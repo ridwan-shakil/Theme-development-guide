adfd8c6a3c2d3c07a450568e5bb950abb6725c50
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-10-21 21:36:32 +0600

    Create woocommerce.php

---
+++ Woocommerce/woocommerce.php
@@ -0,0 +1,8 @@
+This 'woocommerce.php' file will load :
+        1. Shop page,
+        2. product search page,
+        3. product category page,
+        4. single product’s page.
+
+<!-- this line of code will load all the woocommerce contents for those pages  -->
+<?php woocommerce_content(); ?>
