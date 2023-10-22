49223acdd576235eeade7da589707af231651c02
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-10-22 11:24:52 +0600

    Update functions.php

--- Woocommerce/functions.php
+++ Woocommerce/functions.php
@@ -2,6 +2,25 @@
 // ( cart, checkout, account ) pages use your themes page.php file 
 // ( Shop , single product , taxonomy pages = categories, tags )  pages use WordPress own templates 
 
+// Documentation : https://woocommerce.com/document/woocommerce-theme-developer-handbook/
+
+## 3 main ways of woocommerce development :
+	1. Overriding woocommerce templates in your theme
+	2. Using hooks = remove default action and add your action
+	3. creating woocommerce.php file 
+
+// 1. Overriding woocommerce templates in your theme :
+	* Warning: Do not delete any of WooCommerce hooks when overriding a template. This would prevent plugins hooking in to add content.
+	* Overrinding default template requires MAINTANANCE 
+	
+// 2. Using hooks = remove default action and add your action :
+	* It is recommended way , because it usually does not require maintenance 
+ 	
+
+// 3. creating woocommerce.php file
+	*  woocommerce.php has priority over archive-product.php
+	*  you will not be able to override the archive-product.php template 
+
 
 // ==========================
 // Give woocommerce support ***
