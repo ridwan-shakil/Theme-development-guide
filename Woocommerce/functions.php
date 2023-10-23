d5d5460749fe4e5bf65b706e079ffe3804980d28
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-10-23 22:17:53 +0600

    Update functions.php

--- Woocommerce/functions.php
+++ Woocommerce/functions.php
@@ -71,12 +71,34 @@ if (!function_exists('loop_columns')) {
 }
 add_filter('loop_shop_columns', 'loop_columns', 999);
 
-//Remove any post from the shop page 
+/**
+ * remove any product from the shop page 
+ */
 function organi_remove_some_product($wq) {
     $wq->set('post__not_in', array(35));
     return $wq;
 }
 add_filter( 'woocommerce_product_query','organi_remove_some_product' );
+
+/**
+* Remove any product category from the shop page 
+*/
+function organi_remove_some_category($wq) {
+    $tax_query = (array) $wq->get('tax_query');
+    $tax_query[] = array(
+        'taxonomy' => 'product_cat',
+        'field'=> 'slug',
+        'terms'=> array('Tshirts'),
+        'operator'=> 'NOT IN',
+    );
+    $wq->set('tax_query', $tax_query);
+    return $wq;
+}
+add_filter( 'woocommerce_product_query','organi_remove_some_category' );
+
+
+
+
 
 
 // ==========================
