5d204252056f86c66eaf196d225227e17ac54beb
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-11-07 22:01:05 +0600

    Create Notes.php

---
+++ Theme Customizer/Notes.php
@@ -0,0 +1,6 @@
+Theme Customizer: The Theme Customizer is a user-friendly, built-in feature in WordPress. It allows users to customize various aspects of a theme.
+
+
+Kirki : it's a popular third-party toolkit for building advanced theme customizer options.
+Kirki simplifies and extends the capabilities of the WordPress Theme Customizer by
+providing a well-structured framework and additional customizer controls.
